27c9de96968d0433935a092285b0d4b40b57c6ce
ignacy <ignacybanet@cyfroweszkoly.pl>
2025-06-15 22:48:17 +0200

    create basic xml layout

--- app/src/main/java/com/example/personalityquiz/MainActivity.kt
+++ app/src/main/java/com/example/personalityquiz/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.personalityquiz
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.personalityquiz.R.id.mainLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(mainLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
